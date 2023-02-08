9297bee5c7b46da59d367fde122a3a06d0051682
Øyvind <oyvind.w.petersen@ntnu.no>
2023-02-08 08:34:21 +0100

    Tidy

--- bridgebox/LineIntersect.m
+++ bridgebox/LineIntersect.m
@@ -1,24 +1,23 @@
 function [t_a,t_b,I_a,I_b]=LineIntersect(N_a,N_b,n_a,n_b)
-
 %% Calculate intersection of two lines 
-
+%
 % Parametrized vectors:
 % v_a=N_a+t_a*n_a
 % v_b=N_b+t_b*n_b
-% Solve v_a=v_b
-
+% Solve v_a=v_b for t_a and t_b
+%
 % Inputs:
 % N_a: coordinates of node 1 
 % N_b: coordinates of node 2 
 % n_a: direction of vector 1
 % n_b: direction of vector 2
-
+%
 % Outputs:
 % I_a: coordinates intersection
 % I_b: coordinates intersection
-
+%
 % I_a and I_b should be equal
-
+%
 %% Find intersection
 
 A=[-n_a(1) n_b(1) ; -n_a(2) n_b(2) ];
